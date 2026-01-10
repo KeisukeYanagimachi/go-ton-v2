2e23d23d5852a1a0bf72652803e33fd6673a6108
Keisuke Yanagimachi <willowtown0576@gmail.com>
2026-01-10 17:09:17 +0900

    Backfill ticket exam version in migration

--- prisma/migrations/20260110120000_add_ticket_exam_version/migration.sql
+++ prisma/migrations/20260110120000_add_ticket_exam_version/migration.sql
@@ -1,5 +1,16 @@
--- Add exam_version_id to tickets
-ALTER TABLE "tickets" ADD COLUMN "exam_version_id" UUID NOT NULL;
+-- Add exam_version_id to tickets (backfill then set NOT NULL)
+ALTER TABLE "tickets" ADD COLUMN "exam_version_id" UUID;
+
+UPDATE "tickets"
+SET "exam_version_id" = (
+  SELECT "id"
+  FROM "exam_versions"
+  WHERE "status" = 'PUBLISHED'
+  ORDER BY "version_number" DESC, "created_at" DESC
+  LIMIT 1
+);
+
+ALTER TABLE "tickets" ALTER COLUMN "exam_version_id" SET NOT NULL;
 
 -- Add relation to exam_versions
 ALTER TABLE "tickets"
